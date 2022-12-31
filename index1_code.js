75c8b7853928c4b4c1dbd7fa10776a051dbfaa1b
parrydog <kubus.rusinek@gmail.com>
2022-12-31 02:47:08 +0100

    Commented out mouse trailer logic Added HTML preview basic logic Added functional single-object text rotation logic

--- index1_code.js
+++ index1_code.js
@@ -1,46 +1,62 @@
-const trailer = document.getElementById("mouse-trailer");
+// const trailer = document.getElementById("mouse-trailer");
 
-const animateTrailer = (e, interacting) => {
-    const x = e.clientX - trailer.offsetWidth / 2; 
-    const y = e.clientY - trailer.offsetHeight / 2;
+// const animateTrailer = (e, interacting) => {
+//     const x = e.clientX - trailer.offsetWidth / 2; 
+//     const y = e.clientY - trailer.offsetHeight / 2;
 
-    const keyframes = {
-        transform : `translate(${x}px, ${y}px) scale(${interacting ? 6 : 1})`
-    }
+//     const keyframes = {
+//         transform : `translate(${x}px, ${y}px) scale(${interacting ? 6 : 1})`
+//     }
 
-    trailer.animate(keyframes, {
-        duration: 800,
-        fill: "forwards",
-    });
+//     trailer.animate(keyframes, {
+//         duration: 800,
+//         fill: "forwards",
+//     });
+// }
+
+// const getTrailerClass = type => {
+//     switch(type) {
+//         case "html-code":
+//             //<i class="fa-solid fa-code-fork"></i>
+//             return "fa-solid fa-code-fork";
+//         case "more-info":
+//             //<i class="fa-solid fa-arrows-to-eye"></i>
+//             return "fa-solid fa-arrows-to-eye";
+//         default:
+//             //"fa-solid fa-arrow-up-right-from-square"
+//             return "fa-solid fa-arrow-up-right-from-square";
+//     }
+// }
+
+// window.onmousemove = e => {
+//     const interactable = e.target.closest(".mid-scroll"), //change to specific choose subelement
+//         interacting = interactable !== null;
+
+
+//     const icon = document.getElementById("trailer-icon");
+
+//     animateTrailer(e, interacting);
+
+//     if (interacting) {
+//         icon.className = getTrailerClass(interactable.dataset.type);
+//         trailer.style.opacity = 1;
+//     }
+//     else{
+//         icon.className = "";
+//         trailer.style.opacity = 0;
+//     }
+// }
+
+
+const toggleHTMLPreview = () => {
+    document.getElementById("greeter-text").dataset.preview = document.getElementById("greeter-text").dataset.preview === "true" ? "false" : "true";
 }
 
-const getTrailerClass = type => {
-    switch(type) {
-        case "html-code":
-            //<i class="fa-solid fa-code-fork"></i>
-            return "fa-solid fa-code-fork";
-        case "more-info":
-            //<i class="fa-solid fa-arrows-to-eye"></i>
-            return "fa-solid fa-arrows-to-eye";
-        default:
-            //"fa-solid fa-arrow-up-right-from-square"
-            return "fa-solid fa-arrows-to-eye";
-    }
-}
+const circleText = document.querySelector(".html-preview p");
 
-window.onmousemove = e => {
-    const interactable = e.target.closest(".mid-scroll"),
-        interacting = interactable !== null;
+circleText.innerHTML = circleText.innerText.split("").map(
+    (char, i) =>
+    `<span style="transform:rotate(${i * 10 - 50}deg)">${char}</span>`
+).join("")
 
-
-    const icon = document.getElementById("trailer-icon");
-
-    animateTrailer(e, interacting);
-
-    if (interacting) {
-        icon.className = getTrailerClass(interactable.dataset.type);
-    }
-    else{
-        icon.className = "";
-    }
-}
+// make all elements that match .html-preview rotate each span
