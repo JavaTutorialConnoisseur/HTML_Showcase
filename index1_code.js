e71e068732a4a6fc205aad77181d23e816d9c414
parrydog <kubus.rusinek@gmail.com>
2023-01-09 13:40:27 +0100

    Reformatted commented mouse trailer code
    Added basic catching and pushing for HTML preview toggler button
    
    Added Anime.js library and beautification stuff

--- index1_code.js
+++ index1_code.js
@@ -1,13 +1,10 @@
 // const trailer = document.getElementById("mouse-trailer");
-
 // const animateTrailer = (e, interacting) => {
 //     const x = e.clientX - trailer.offsetWidth / 2; 
 //     const y = e.clientY - trailer.offsetHeight / 2;
-
 //     const keyframes = {
 //         transform : `translate(${x}px, ${y}px) scale(${interacting ? 6 : 1})`
 //     }
-
 //     trailer.animate(keyframes, {
 //         duration: 800,
 //         fill: "forwards",
@@ -24,19 +21,15 @@
 //             return "fa-solid fa-arrows-to-eye";
 //         default:
 //             //"fa-solid fa-arrow-up-right-from-square"
-//             return "fa-solid fa-arrow-up-right-from-square";
+//             return "";
 //     }
 // }
 
 // window.onmousemove = e => {
 //     const interactable = e.target.closest(".mid-scroll"), //change to specific choose subelement
 //         interacting = interactable !== null;
-
-
 //     const icon = document.getElementById("trailer-icon");
-
 //     animateTrailer(e, interacting);
-
 //     if (interacting) {
 //         icon.className = getTrailerClass(interactable.dataset.type);
 //         trailer.style.opacity = 1;
@@ -47,16 +40,51 @@
 //     }
 // }
 
+    // HTML preview
 
 const toggleHTMLPreview = () => {
-    document.getElementById("greeter-text").dataset.preview = document.getElementById("greeter-text").dataset.preview === "true" ? "false" : "true";
+    document.getElementById("greeter-text").dataset.previewHTML = document.getElementById("greeter-text").dataset.previewHTML === "true" ? "false" : "true";
+    
+    var greeter = document.getElementById('greeter-text'); 
+
+    let midDiv = document.createElement("div");
+    midDiv.classList.add('mid-scroll-sub');
+
+    const buttonsGreeter = document.querySelectorAll('#greeter-text button');
+    
+    for (let i = 0; i < buttonsGreeter.length; ++i) {
+        midDiv.appendChild(buttonsGreeter[i]);
+    }
+
+    midDiv.style.justifyContent = 'space-between';
+    midDiv.style.margin = '1% 0';
+
+    greeter.style.flexDirection = 'column';
+    greeter.style.padding = '2% 0';
+    greeter.prepend(midDiv);
+
+    anime({
+        targets: '',
+        easing: 'linear'
+    });
 }
 
-const circleText = document.querySelector(".html-preview p");
+const circleTextHTML = document.querySelector(".html-preview p");
 
-circleText.innerHTML = circleText.innerText.split("").map(
+circleTextHTML.innerHTML = circleTextHTML.innerText.split("").map(
     (char, i) =>
     `<span style="transform:rotate(${i * 10 - 50}deg)">${char}</span>`
-).join("")
+).join("");
 
-// make all elements that match .html-preview rotate each span
+    // Additional text preview
+
+const toggleMoreText = () => {
+    document.getElementById("greeter-text").dataset.previewTEXT = document.getElementById("greeter-text").dataset.previewTEXT === "true" ? "false" : "true";
+}
+
+const circleTextMORE = document.querySelector(".text-preview p");
+
+circleTextMORE.innerHTML = circleTextMORE.innerText.split("").map(
+    (char, i) =>
+    `<span style="transform:rotate(${i * 10 - 50}deg)">${char}</span>`
+).join("");
